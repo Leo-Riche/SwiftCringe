864d1c9f936d89e709f30be4508a3f7e0e3b8578
Leo-Riche <richeleo@icloud.com>
2025-11-04 16:48:48 +0100

    Initial Commit

---
+++ SwiftCringe/SwiftCringeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCringeApp.swift
+//  SwiftCringe
+//
+//  Created by RICHÉ Léo on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCringeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
